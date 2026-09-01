904f9b4b4c2c15aa33111835d1c87f8d73164e77
hoangKhuong55551 <khuong55551@gmail.com>
2026-09-01 21:06:56 +0700

    don file rac

--- fix_ticket.py
+++
@@ -1,9 +0,0 @@
-﻿with open('templates/bookings/ticket.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('Gh? <strong', 'Ghế <strong')
-content = content.replace('>D?i gh?</a>', '>Đổi ghế</a>')
-content = content.replace('Gh <strong', 'Ghế <strong')
-
-with open('templates/bookings/ticket.html', 'w', encoding='utf-8') as f:
-    f.write(content)
